fa03509bfa16a7d910ef1c282df406474f0bfa9e
kavyapatel-10 <kavyapatel1234575@gmail.com>
2026-07-18 21:44:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (21.78%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        
+        long long revnum = 0;
+        int dup = x;
+        while(x>0){
+
+          int ld = x % 10;
+         
+          revnum = revnum * 10 + ld;  
+
+           x = x/10;
+        }
+
+        if(revnum == dup){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
